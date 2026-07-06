ba0bf909dc6529a9db680a5fe1812e78e6fe5935
Tauqueer Alam <tauqueeralam100@gmail.com>
2026-07-06 11:09:30 +0530

    Time: 6 ms (7.32%), Space: 12.5 MB (17.66%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortColors(self, nums):
+        for i in range(len(nums)):
+            for j in range(len(nums)-1-i):
+                if nums[j]>nums[j+1]:
+                    temp=nums[j]
+                    nums[j]=nums[j+1]
+                    nums[j+1]=temp
+        return nums    
+
+        
