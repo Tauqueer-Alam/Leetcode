864246556055674947ebc7329b6450b7a23a388f
Tauqueer Alam <tauqueeralam100@gmail.com>
2026-07-13 19:31:24 +0530

    Time: 10 ms (54.3%), Space: 13.5 MB (73.24%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        freq={}
+        for i in nums:
+            if i in freq:
+                freq[i]=freq[i]+1
+            else:
+                freq[i]=1  
+
+
+        return max(freq,key=freq.get)  
+
+
+        
