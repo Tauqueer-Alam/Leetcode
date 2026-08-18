cfd5c7da64f935f289a5f1fbae7298cf5c1c9d24
Tauqueer Alam <tauqueeralam100@gmail.com>
2026-08-18 11:05:33 +0530

    Time: 0 ms (100%), Space: 35.8 MB (55.68%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int total_sum=0;
+        for (int i=0;i<n;i++){
+            total_sum+=nums[i];
+        }
+        
+        int left_sum=0;
+        for (int i=0;i<n;i++){
+            int right_sum=total_sum-nums[i]-left_sum;
+            if (left_sum==right_sum){
+                return i;
+            }
+            left_sum+=nums[i];
+        }
+
+        return -1;
+        
+
+
+    }
+};
