0f91b761486d6dfecc11eb864000b67b90c4fe7d
Tauqueer Alam <tauqueeralam100@gmail.com>
2026-08-19 14:28:46 +0530

    Time: 223 ms (5%), Space: 21.8 MB (71.22%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        for (int i=0; i<=n; i++){
+            if(find(nums.begin(),nums.end(),i)==nums.end()){
+                return i;
+            }
+
+        }
+        return -1;
+        
+    }
+};
