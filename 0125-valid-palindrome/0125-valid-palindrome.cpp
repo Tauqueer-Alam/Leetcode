1c72e54f9e7d7c90c504062eab57e437bc896e13
Tauqueer Alam <tauqueeralam100@gmail.com>
2026-09-05 21:08:12 +0530

    Time: 0 ms (100%), Space: 9.9 MB (52.71%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isAlphanum(char ch){
+        if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+    bool isPalindrome(string s) {
+        int n=s.length();
+        int left=0;
+        int right=n-1;
+
+        while(left<right){
+            if(!isAlphanum(s[left])){
+                left++;
+                continue;
+            }
+            if(!isAlphanum(s[right])){
+                right--;
+                continue;
+            }
+
+            if(tolower(s[left])!=tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+        
+    }
+};
