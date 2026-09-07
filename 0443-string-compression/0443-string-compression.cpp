276b2bce87b19c0b1b15c4e8dac49ddd6c913579
Tauqueer Alam <tauqueeralam100@gmail.com>
2026-09-07 19:38:34 +0530

    Time: 0 ms (100%), Space: 13.7 MB (76.84%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n=chars.size();
+        int index=0;
+        for(int i=0;i<n;i++){
+            int ch=chars[i];
+            int count=0;
+            while(i<n && chars[i]==ch){
+                count++;
+                i++;
+            }
+            if(count==1){
+                chars[index++]=ch;
+            }
+            else{
+                chars[index++]=ch;
+                string str=to_string(count);
+                for(int dig:str){
+                    chars[index++]=dig;
+                }        
+                
+            }
+            i--;
+
+
+        }
+        chars.resize(index);
+        return index;
+        
+    }
+};
